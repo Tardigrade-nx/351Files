48dff1e2ff6100e3441bd9fbd32a23d5eebf2149
Tardigrade-nx <dsfgfhj@yahoo.fr>
2021-07-18 21:18:55 +0200

    Text Editor: copy/cut/paste selected text.

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,7 @@ SDL_Texture *g_iconCancel = NULL;
 SDL_Texture *g_iconFloppy = NULL;
 SDL_Texture *g_iconImage = NULL;
 SDL_Texture *g_iconFileText = NULL;
+SDL_Texture *g_iconPlus = NULL;
 
 //------------------------------------------------------------------------------
 
@@ -66,6 +67,7 @@ int main(int argc, char* args[])
    g_iconFloppy = SDLUtils::loadTexture(std::string(RES_PATH) + "/floppy.png");
    g_iconImage = SDLUtils::loadTexture(std::string(RES_PATH) + "/image.png");
    g_iconFileText = SDLUtils::loadTexture(std::string(RES_PATH) + "/file-text.png");
+   g_iconPlus = SDLUtils::loadTexture(std::string(RES_PATH) + "/plus.png");
 
    // Load fonts
    g_font = SDLUtils::loadFont(std::string(RES_PATH) + "/" + FONT_NAME, FONT_SIZE);
@@ -109,6 +111,7 @@ int main(int argc, char* args[])
    if (g_iconFloppy != NULL)     { SDL_DestroyTexture(g_iconFloppy);    g_iconFloppy = NULL; }
    if (g_iconImage != NULL)      { SDL_DestroyTexture(g_iconImage);     g_iconImage = NULL; }
    if (g_iconFileText != NULL)   { SDL_DestroyTexture(g_iconFileText);  g_iconFileText = NULL; }
+   if (g_iconPlus != NULL)       { SDL_DestroyTexture(g_iconPlus);      g_iconPlus = NULL; }
 
    // Quit SDL
    SDLUtils::close();
